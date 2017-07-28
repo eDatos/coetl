1616ab6e58b11ae763b73c1276573f6472bdba4e
Zebensui Méndez Corujo <zmencor@arte-consultores.com>
2017-07-28 12:12:51 +0100

    SECRETARIA-7 - Configurar entorno de desarrollo local y crear el proyecto base

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -6,11 +6,10 @@ DEPLOY_TARGET_PATH=/servers/secretaria/tomcats/secretaria01/webapps
 DEMO_ENV=$HOME_PATH/env
 
 scp -r etc/deploy deploy@secretaria.arte:$TRANSFER_PATH
-scp target/secretaria-*.war deploy@secretaria.arte:$TRANSFER_PATH/secretaria.war
+scp target/secretaria-*.war deploy@secretaria.arte:$TRANSFER_PATH/secretaria-libro.war
 ssh deploy@secretaria.arte <<EOF
 
     chmod a+x $TRANSFER_PATH/deploy/*.sh;
-    . $TRANSFER_PATH/deploy/utilities.sh
     
     sudo service secretaria01 stop
     
@@ -20,14 +19,14 @@ ssh deploy@secretaria.arte <<EOF
 
     # Update Process
     sudo rm -rf $DEPLOY_TARGET_PATH/ROOT
-    sudo mv $TRANSFER_PATH/secretaria.war $DEPLOY_TARGET_PATH/secretaria.war
-    sudo unzip $DEPLOY_TARGET_PATH/secretaria.war -d $DEPLOY_TARGET_PATH/ROOT
-    sudo rm -rf $DEPLOY_TARGET_PATH/secretaria.war
+    sudo mv $TRANSFER_PATH/secretaria-libro.war $DEPLOY_TARGET_PATH/secretaria-libro.war
+    sudo unzip $DEPLOY_TARGET_PATH/secretaria-libro.war -d $DEPLOY_TARGET_PATH/secretaria-libro
+    sudo rm -rf $DEPLOY_TARGET_PATH/secretaria-libro.war
 
     # Restore Configuration
-    sudo cp $DEMO_ENV/logback.xml $DEPLOY_TARGET_PATH/ROOT/WEB-INF/classes/
-    sudo rm -f $DEPLOY_TARGET_PATH/ROOT/WEB-INF/classes/config/application-env.yml
-    sudo cp $DEMO_ENV/data-location.properties $DEPLOY_TARGET_PATH/ROOT/WEB-INF/classes/config/
+    sudo cp $DEMO_ENV/logback.xml $DEPLOY_TARGET_PATH/secretaria-libro/WEB-INF/classes/
+    sudo rm -f $DEPLOY_TARGET_PATH/secretaria-libro/WEB-INF/classes/config/application-env.yml
+    sudo cp $DEMO_ENV/data-location.properties $DEPLOY_TARGET_PATH/secretaria-libro/WEB-INF/classes/config/
     
     sudo chown -R secretaria.secretaria /servers/secretaria
     sudo service secretaria01 start
