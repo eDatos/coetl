878a1e9c7082373e0325bcd817f21c686ab5912e
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2017-09-05 15:41:58 +0100

    SECRETARIA-40 - Componente: Pantalla de listado de usuarios > Refactor de sticky-table-header para simplificar la api

--- src/main/webapp/app/shared/sticky-table-header/sticky-table-header.directive.ts
+++ src/main/webapp/app/shared/sticky-table-header/sticky-table-header.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef, AfterViewInit, Input } from '@angular/core';
+import { Directive, ElementRef, AfterViewInit } from '@angular/core';
 import * as $ from 'jquery';
 
 @Directive({
@@ -6,18 +6,12 @@ import * as $ from 'jquery';
 })
 export class StickyTableHeaderDirective implements AfterViewInit {
 
-    @Input()
-    stickyPreviousElement: string;
-
     constructor(
         private el: ElementRef
     ) { }
 
     ngAfterViewInit() {
-        const $previousElement = $(this.stickyPreviousElement);
-        if ($previousElement.length) {
-            const offset = $previousElement[0].getBoundingClientRect().bottom;
-            $(this.el.nativeElement).stickyTableHeaders({ fixedOffset: offset });
-        }
+        const offset = $(this.el.nativeElement)[0].getBoundingClientRect().top;
+        $(this.el.nativeElement).stickyTableHeaders({ fixedOffset: offset });
     }
 }
