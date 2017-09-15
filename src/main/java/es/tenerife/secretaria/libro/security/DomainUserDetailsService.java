76837df4e498faa64d8ac8be4ed5efbf6a523d8d
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2017-09-15 16:43:33 +0100

    Revert "SECRETARIA-36 - Gestión dinámica de los roles y permisos asignados a usuarios" Revert parcial para recuperar DomainUserDetailsService.java This reverts commit a2167883682d2694cecd3011c46b6facbb050ff9.

---
+++ src/main/java/es/tenerife/secretaria/libro/security/DomainUserDetailsService.java
@@ -0,0 +1,52 @@
+package es.tenerife.secretaria.libro.security;
+
+import java.util.List;
+import java.util.Locale;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import es.tenerife.secretaria.libro.domain.Usuario;
+import es.tenerife.secretaria.libro.repository.UsuarioRepository;
+
+/**
+ * Authenticate a user from the database.
+ */
+@Component("userDetailsService")
+public class DomainUserDetailsService implements UserDetailsService {
+
+	private final Logger log = LoggerFactory.getLogger(DomainUserDetailsService.class);
+
+	private final UsuarioRepository userRepository;
+
+	public DomainUserDetailsService(UsuarioRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(final String login) {
+		log.debug("Authenticating {}", login);
+		String lowercaseLogin = login.toLowerCase(Locale.ENGLISH);
+		Optional<Usuario> userFromDatabase = userRepository
+				.findOneWithRolesByLoginAndDeletionDateIsNull(lowercaseLogin);
+		return userFromDatabase.map(user -> {
+			if (!user.getActivado()) {
+				throw new UserNotActivatedException("User " + lowercaseLogin + " was not activated");
+			}
+			List<GrantedAuthority> grantedAuthorities = user.getRoles().stream()
+					.map(authority -> new SimpleGrantedAuthority(authority.getNombre())).collect(Collectors.toList());
+			return new org.springframework.security.core.userdetails.User(lowercaseLogin, "", grantedAuthorities);
+		}).orElseThrow(
+				() -> new UsernameNotFoundException("User " + lowercaseLogin + " was not found in the " + "database"));
+	}
+}
