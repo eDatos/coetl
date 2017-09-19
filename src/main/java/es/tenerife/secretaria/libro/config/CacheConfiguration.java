bd613a022e7db72a2cb71c470178247e9dcf9511
Fernando M. Gonzalez Morales <fgonmor@arte-consultores.com>
2017-09-19 13:22:18 +0100

    SECRETARIA-52 - Deshabilitar caché para evitar problemas de clúster

--- src/main/java/es/tenerife/secretaria/libro/config/CacheConfiguration.java
+++ src/main/java/es/tenerife/secretaria/libro/config/CacheConfiguration.java
@@ -7,19 +7,15 @@ import org.ehcache.config.builders.ResourcePoolsBuilder;
 import org.ehcache.expiry.Duration;
 import org.ehcache.expiry.Expirations;
 import org.ehcache.jsr107.Eh107Configuration;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.cache.JCacheManagerCustomizer;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import io.github.jhipster.config.JHipsterProperties;
 
-@Configuration
-@EnableCaching
-@AutoConfigureAfter(value = { MetricsConfiguration.class })
-@AutoConfigureBefore(value = { WebConfigurer.class, DatabaseConfiguration.class })
+//@Configuration
+//@EnableCaching
+//@AutoConfigureAfter(value = { MetricsConfiguration.class })
+//@AutoConfigureBefore(value = { WebConfigurer.class, DatabaseConfiguration.class })
 public class CacheConfiguration {
 
 	private final javax.cache.configuration.Configuration<Object, Object> jcacheConfiguration;
