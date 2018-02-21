41a64cf4b5349ba76e1e762a9c3f183eb70ff4d2
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2018-02-21 11:40:01 +0000

    INFRASTR-58 - Instalación de arte-application-template en tacoronte > Actualizando deploy para tacoronte

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -1,37 +1,37 @@
 #!/bin/sh
 
-# FIXME: Eliminar referencias a la plantilla (com.arte.application.template, arte-application-template, etc...)
+# FIXME: Eliminar referencias a la plantilla (com.arte.application.template, arte-application-template, plantilla, etc...)
 
-HOME_PATH=arte-application-template
+HOME_PATH=plantilla
 TRANSFER_PATH=$HOME_PATH/tmp
-DEPLOY_TARGET_PATH=/servers/arte-application-template/tomcats/arte-application-template01/webapps
+DEPLOY_TARGET_PATH=/servers/plantilla/tomcats/plantilla01/webapps
 DEMO_ENV=$HOME_PATH/env
 
-scp -r etc/deploy deploy@arte-application-template.arte:$TRANSFER_PATH
-scp target/arte-*.war deploy@arte-application-template.arte:$TRANSFER_PATH/arte-application-template.war
-ssh deploy@arte-application-template.arte <<EOF
+scp -r etc/deploy deploy@tacoronte.arte:$TRANSFER_PATH
+scp target/arte-application-template-*.war deploy@tacoronte.arte:$TRANSFER_PATH/plantilla.war
+ssh deploy@tacoronte.arte <<EOF
 
     chmod a+x $TRANSFER_PATH/deploy/*.sh;
     
-    sudo service arte-application-template01 stop
+    sudo service plantilla01 stop
     
     ###
     # ARTE-APPLICATION-TEMPLATE
     ###
 
     # Update Process
-    sudo rm -rf $DEPLOY_TARGET_PATH/arte-application-template
-    sudo mv $TRANSFER_PATH/arte-application-template.war $DEPLOY_TARGET_PATH/arte-application-template.war
-    sudo unzip $DEPLOY_TARGET_PATH/arte-application-template.war -d $DEPLOY_TARGET_PATH/arte-application-template
-    sudo rm -rf $DEPLOY_TARGET_PATH/arte-application-template.war
+    sudo rm -rf $DEPLOY_TARGET_PATH/plantilla
+    sudo mv $TRANSFER_PATH/plantilla.war $DEPLOY_TARGET_PATH/plantilla.war
+    sudo unzip $DEPLOY_TARGET_PATH/plantilla.war -d $DEPLOY_TARGET_PATH/plantilla
+    sudo rm -rf $DEPLOY_TARGET_PATH/plantilla.war
 
     # Restore Configuration
-    sudo cp $DEMO_ENV/logback.xml $DEPLOY_TARGET_PATH/arte-application-template/WEB-INF/classes/
-    sudo rm -f $DEPLOY_TARGET_PATH/arte-application-template/WEB-INF/classes/config/application-env.yml
-    sudo cp $DEMO_ENV/data-location.properties $DEPLOY_TARGET_PATH/arte-application-template/WEB-INF/classes/config/
+    sudo cp $DEMO_ENV/logback.xml $DEPLOY_TARGET_PATH/plantilla/WEB-INF/classes/
+    sudo rm -f $DEPLOY_TARGET_PATH/plantilla/WEB-INF/classes/config/application-env.yml
+    sudo cp $DEMO_ENV/data-location.properties $DEPLOY_TARGET_PATH/plantilla/WEB-INF/classes/config/
     
-    sudo chown -R arte-application-template.arte-application-template /servers/arte-application-template
-    sudo service arte-application-template01 start
+    sudo chown -R plantilla.plantilla /servers/plantilla
+    sudo service plantilla01 start
     
 
     echo "Finished deploy"
