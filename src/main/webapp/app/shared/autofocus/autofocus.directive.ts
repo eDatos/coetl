080f752c451c5be5f9d9faf7f64caab3dfcdc70e
Javier Villamonte <jvilper@arte-consultores.com>
2018-03-26 11:26:59 +0100

    INFRASTR-93 - Debe hacerse focos en los elementos marcados con autofocus aunque esten deshabilitados
    >Se traen los cambios del fichero autofocus.directive.ts en el commit http://git.arte-consultores.com/gobcan/agrirp/commit/85b1cee15226015ab831fba12bb1a0c801ff9c8b
    >Se quita el 'else if' porque es un caso que nunca se va a dar.
    >Se invoca directamente el método 'focus()' en el elemento porque 'this.createFocusableSibling(elementToFocus);' hace un elementToFocus.text = '', eliminando su contenido.
    >Se quita el 'if (focusableSibling)'. Es un cambio de código posterior en AGRIRP.

--- src/main/webapp/app/shared/autofocus/autofocus.directive.ts
+++ src/main/webapp/app/shared/autofocus/autofocus.directive.ts
@@ -3,16 +3,17 @@ import { AfterViewInit, Directive, ElementRef, Renderer2 } from '@angular/core';
 // Forked from https://shekhargulati.com/2017/12/02/adding-autofocus-to-an-input-field-in-an-angular-5-bootstrap-4-application/
 
 /**
- * Directiva para realizar focus al primer elemento no desactivado y que sea editable.
- * Soporta la inclusión de este atriubto en un elemento editable
+ * Directiva para realizar focus al primer elemento no desactivado y que sea editable. Si ninguno cumple la condición se creará un hermano invisible al que se le hará focus
+ * Soporta la inclusión de este atributo en un elemento 'focusable'
  * (<input>, <textarea>, <ac-autocomplete>, <ac-calendar>, etc...); así como en un <fieldset>.
+ * NO debe usarse dentro de un fieldset disabled. En ese caso, la directiva siempre debe estar asignada al fieldset
 */
 @Directive({
     selector: '[acAutofocus]'
 })
 export class AutofocusDirective implements AfterViewInit {
 
-    readonly editableElements: string[] = ['input', 'textarea'];
+    readonly focusableElements: string[] = ['input', 'textarea'];
 
     constructor(private el: ElementRef, private renderer: Renderer2) {
     }
@@ -25,18 +26,30 @@ export class AutofocusDirective implements AfterViewInit {
     }
 
     private focusOnFirstEditableElement(htmlElement: any) {
-        if (htmlElement.disabled) { return; }
-
-        const candidateChilds: HTMLElement[] = this.querySelectorAllEditableElements(htmlElement);
+        const candidateChilds: HTMLElement[] = this.querySelectorAllFocusableElements(htmlElement);
         const firstEnabledChild: HTMLElement = candidateChilds.find((element: any) => {
             return !element.disabled;
         });
 
-        const elementToFocus = firstEnabledChild || htmlElement;
-        this.renderer.selectRootElement(elementToFocus).focus();
+        let elementToFocus = firstEnabledChild || htmlElement;
+        // elementToFocus.disabled devuelve false aun dentro de un fieldset disabled, pero luego no nos deja hacer focus. Por tanto
+        let focusableSibling = false;
+        if (htmlElement.disabled) {
+            focusableSibling = true;
+            elementToFocus = this.createFocusableSibling(htmlElement);
+        }
+
+        elementToFocus.focus();
     }
 
-    private querySelectorAllEditableElements(htmlElement: HTMLElement): HTMLElement[] {
-        return Array.prototype.slice.call(htmlElement.querySelectorAll(this.editableElements.join(', ')));
+    private createFocusableSibling(node) {
+        const sibling = this.renderer.createElement('div');
+        this.renderer.setAttribute(sibling, 'tabindex', '0');
+        this.renderer.appendChild(this.renderer.parentNode(node), sibling);
+        return sibling;
+    }
+
+    private querySelectorAllFocusableElements(htmlElement: HTMLElement): HTMLElement[] {
+        return Array.prototype.slice.call(htmlElement.querySelectorAll(this.focusableElements.join(', ')));
     }
 }
