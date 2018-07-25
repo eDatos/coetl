d951a1ecbbd0a34c20991525953033c1bb204560
Miguel Cabrera <mcabsan@arte-consultores.com>
2018-07-25 15:25:56 +0100

    COETL-31 - Consulta y búsqueda de ETL
    > Creada nuevas clases abstractas, AbstractAuditingWithDeletionEntity
    y AbstractVersionedAndAuditingWithDeletionEntity, para gestionar
    la auditoría de borrado.

---
+++ src/main/java/es/gobcan/coetl/domain/AbstractAuditingWithDeletionEntity.java
@@ -0,0 +1,45 @@
+package es.gobcan.coetl.domain;
+
+import java.time.ZonedDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.hibernate.envers.Audited;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+/**
+ * Base abstract class for entities which will hold definitions for created,
+ * last modified by and created, last modified by date.
+ */
+@MappedSuperclass
+@Audited
+@EntityListeners(AuditingEntityListener.class)
+public abstract class AbstractAuditingWithDeletionEntity extends AbstractAuditingEntity {
+
+    private static final long serialVersionUID = -4785510038403987259L;
+
+    @Column(name = "deletion_date")
+    private ZonedDateTime deletionDate;
+
+    @Column(name = "deleted_by", length = 50)
+    private String deletedBy;
+
+    public ZonedDateTime getDeletionDate() {
+        return deletionDate;
+    }
+
+    public void setDeletionDate(ZonedDateTime deletionDate) {
+        this.deletionDate = deletionDate;
+    }
+
+    public String getDeletedBy() {
+        return deletedBy;
+    }
+
+    public void setDeletedBy(String deletedBy) {
+        this.deletedBy = deletedBy;
+    }
+
+}
---
+++ src/main/java/es/gobcan/coetl/domain/AbstractVersionedAndAuditingWithDeletionEntity.java
@@ -0,0 +1,38 @@
+package es.gobcan.coetl.domain;
+
+import java.time.ZonedDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import es.gobcan.coetl.optimistic.OptimisticLockChecker;
+
+@MappedSuperclass
+@EntityListeners(OptimisticLockChecker.class)
+public abstract class AbstractVersionedAndAuditingWithDeletionEntity extends AbstractVersionedAndAuditingEntity {
+
+    private static final long serialVersionUID = 2035689875143963693L;
+
+    @Column(name = "deletion_date")
+    private ZonedDateTime deletionDate;
+
+    @Column(name = "deleted_by", length = 50)
+    private String deletedBy;
+
+    public ZonedDateTime getDeletionDate() {
+        return deletionDate;
+    }
+
+    public void setDeletionDate(ZonedDateTime deletionDate) {
+        this.deletionDate = deletionDate;
+    }
+
+    public String getDeletedBy() {
+        return deletedBy;
+    }
+
+    public void setDeletedBy(String deletedBy) {
+        this.deletedBy = deletedBy;
+    }
+}
