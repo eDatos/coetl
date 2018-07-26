e0e0ff67c1d36ad0a19d682855f4368a61533ddd
Miguel Cabrera <mcabsan@arte-consultores.com>
2018-07-26 11:55:09 +0100

    COETL - 31 - Consulta y búsqueda de ETL
    > Creado recurso y mapper de la entidad Etl.

---
+++ src/main/java/es/gobcan/coetl/web/rest/mapper/EtlMapper.java
@@ -0,0 +1,43 @@
+package es.gobcan.coetl.web.rest.mapper;
+
+import org.apache.commons.lang3.StringUtils;
+import org.mapstruct.Mapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import es.gobcan.coetl.domain.Etl;
+import es.gobcan.coetl.repository.EtlRepository;
+import es.gobcan.coetl.web.rest.dto.EtlDTO;
+
+@Mapper(componentModel = "spring")
+public abstract class EtlMapper implements EntityMapper<EtlDTO, Etl> {
+
+    @Autowired
+    EtlRepository etlRepository;
+
+    Etl fromId(Long id) {
+        return (id == null) ? null : etlRepository.findOne(id);
+    }
+
+    public Etl toEntity(EtlDTO dto) {
+        if (dto == null) {
+            return null;
+        }
+
+        Etl entity = (dto.getId() != null) ? fromId(dto.getId()) : new Etl();
+
+        entity.setCode(dto.getCode());
+        entity.setName(dto.getName());
+        entity.setPurpose((StringUtils.isNotBlank(dto.getPurpose())) ? dto.getPurpose() : null);
+        entity.setOrganizationInCharge(dto.getOrganizationInCharge());
+        entity.setFunctionalInCharge(dto.getFunctionalInCharge());
+        entity.setTechnicalInCharge(dto.getTechnicalInCharge());
+        entity.setType(dto.getType());
+        entity.setComments((StringUtils.isNotBlank(dto.getComments())) ? dto.getComments() : null);
+        entity.setExecutionDescription((StringUtils.isNotBlank(dto.getExecutionDescription())) ? dto.getExecutionDescription() : null);
+        entity.setExecutionPlanning((StringUtils.isNotBlank(dto.getExecutionPlanning())) ? dto.getExecutionPlanning() : null);
+
+        entity.setOptLock(dto.getOptLock());
+
+        return entity;
+    }
+}
