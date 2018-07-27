8441337711679c2c46038046c579f87649122fca
Miguel Cabrera <mcabsan@arte-consultores.com>
2018-07-27 14:43:22 +0100

    COETL-31 - Consulta y búsqueda de ETL
    > Creado componente de eliminación de una Etl

---
+++ src/main/webapp/app/entities/etl/etl-delete-dialog.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { JhiEventManager } from 'ng-jhipster';
+
+import { Etl } from './etl.model';
+import { EtlService } from './etl.service';
+
+@Component({
+    selector: 'ac-etl-delete-dialog',
+    templateUrl: 'etl-delete-dialog.component.html'
+})
+export class EtlDeleteDialogComponent implements OnInit {
+    etl: Etl;
+
+    constructor(
+        private etlService: EtlService,
+        private activeModal: NgbActiveModal,
+        private eventManager: JhiEventManager
+    ) {}
+
+    ngOnInit() {}
+
+    clear() {
+        this.activeModal.dismiss(false);
+    }
+
+    confirmDelete(idEtl: number) {
+        this.etlService.delete(idEtl).subscribe((response) => {
+            this.eventManager.broadcast({
+                name: 'etlListModification',
+                content: response
+            });
+            this.activeModal.close(true);
+        });
+    }
+}
--- src/main/webapp/app/entities/etl/etl.module.ts
+++ src/main/webapp/app/entities/etl/etl.module.ts
@@ -7,13 +7,14 @@ import { etlRoute, EtlResolvePagingParams } from './etl.route';
 import { EtlComponent } from './etl.component';
 import { EtlSearchComponent } from './etl-search';
 import { EtlResolve } from './etl-resolve.service';
+import { EtlDeleteDialogComponent } from './etl-delete-dialog.component';
 
 const ENTITY_STATES = [...etlRoute];
 
 @NgModule({
     imports: [CoetlSharedModule, RouterModule.forRoot(ENTITY_STATES, { useHash: true })],
-    declarations: [EtlComponent, EtlSearchComponent],
-    entryComponents: [],
+    declarations: [EtlComponent, EtlSearchComponent, EtlDeleteDialogComponent],
+    entryComponents: [EtlDeleteDialogComponent],
     providers: [EtlService, EtlResolve, EtlResolvePagingParams]
 })
 export class CoetlEtlModule {}
