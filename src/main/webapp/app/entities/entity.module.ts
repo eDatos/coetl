b47c0c9f299e5e4115206ec3a0bc671f1c36fa41
Miguel Cabrera <mcabsan@arte-consultores.com>
2018-07-27 14:21:36 +0100

    COETL-31 - Consulta y búsqueda de ETL
    > Añadido el módulo de Etl al módulo de entidad.

--- src/main/webapp/app/entities/entity.module.ts
+++ src/main/webapp/app/entities/entity.module.ts
@@ -1,12 +1,14 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 
 import { CoetlDocumentoModule } from './documento/documento.module';
+import { CoetlEtlModule } from './etl/etl.module';
 
 /* jhipster-needle-add-entity-module-import - JHipster will add entity modules imports here */
 
 @NgModule({
     imports: [
-        CoetlDocumentoModule
+        CoetlDocumentoModule,
+        CoetlEtlModule
         /* jhipster-needle-add-entity-module - JHipster will add entity modules here */
     ],
     declarations: [],
