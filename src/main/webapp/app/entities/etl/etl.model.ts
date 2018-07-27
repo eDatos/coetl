cebbf52f984c6fab0374a7b7f71d219a276c39a3
Miguel Cabrera <mcabsan@arte-consultores.com>
2018-07-27 14:17:42 +0100

    COETL-31 - Consulta y búsqueda de ETL
    > Creado modelo de la entidad Etl.
    
    > Creado servicio del cliente del modelo Etl.
    
    > Creado fichero de internacionalización para el modelo Etl.
    
    > Modificado fichero de internacionalización global para añadir la
    entrada del menú a las pantallas de Etl.

---
+++ src/main/webapp/app/entities/etl/etl.model.ts
@@ -0,0 +1,28 @@
+import { BaseVersionedAndAuditingWithDeletionEntity } from '../../shared/model/base-versioned-auditing-with-deletion-entity';
+
+export enum Type {
+    ETL = 'ETL',
+    JOB = 'JOB'
+}
+
+export class Etl extends BaseVersionedAndAuditingWithDeletionEntity {
+    constructor(
+        public id?: number,
+        public code?: string,
+        public name?: string,
+        public purpose?: string,
+        public organizationInCharge?: string,
+        public functionalInCharge?: string,
+        public technicalInCharge?: string,
+        public type?: Type,
+        public comments?: string,
+        public executionDescription?: string,
+        public executionPlanning?: string
+    ) {
+        super();
+    }
+
+    isDeleted(): boolean {
+        return !!this.deletionDate;
+    }
+}
