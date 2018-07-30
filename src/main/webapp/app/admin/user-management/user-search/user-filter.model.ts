0014efa176eec05e53efd1bd40d16041b4f850fe
Miguel Cabrera <mcabsan@arte-consultores.com>
2018-07-30 15:22:50 +0100

    COETL-28 - Autorización
    > Añadido filtro de búsqueda por rol a la entidad Usuario.

--- src/main/webapp/app/admin/user-management/user-search/user-filter.model.ts
+++ src/main/webapp/app/admin/user-management/user-search/user-filter.model.ts
@@ -1,17 +1,22 @@
-import { EntityFilter } from '../../../shared';
+import { EntityFilter, Rol } from '../../../shared';
 
 export class UserFilter implements EntityFilter {
+    allRoles: string[];
 
     public name?: string;
+    public role?: string;
     public includeDeleted = false;
 
-    constructor(
-    ) {
+    constructor() {
+        this.allRoles = Object.keys(Rol);
     }
 
     fromQueryParams(params: any) {
         if (params['name']) {
             this.name = params['name'];
+        }
+        if (params['role']) {
+            this.role = this.convertParamToRole(params['role']);
         }
         if (params['includeDeleted']) {
             this.includeDeleted = params['includeDeleted'] === 'true';
@@ -19,7 +24,8 @@ export class UserFilter implements EntityFilter {
     }
 
     reset() {
-        this.name = '';
+        this.name = null;
+        this.role = null;
         this.includeDeleted = false;
     }
 
@@ -30,16 +36,25 @@ export class UserFilter implements EntityFilter {
     toUrl() {
         return {
             name: this.name,
-            includeDeleted: this.includeDeleted,
-        }
+            role: this.role,
+            includeDeleted: this.includeDeleted
+        };
+    }
+
+    private convertParamToRole(param: any): Rol {
+        const currentKey = this.allRoles.find((key) => key === param);
+        return currentKey ? Rol[currentKey] : undefined;
     }
 
     private getCriterias() {
         const criterias: string[] = [];
         if (this.name) {
-            const subcriterias: string[] = []
-            this.name.split(' ').forEach((item) => subcriterias.push(`USUARIO ILIKE '%${item}%'`))
+            const subcriterias: string[] = [];
+            this.name.split(' ').forEach((item) => subcriterias.push(`USUARIO ILIKE '%${item}%'`));
             criterias.push('(' + subcriterias.join(' AND ') + ')');
+        }
+        if (this.role) {
+            criterias.push(`ROLES EQ '${this.role}'`);
         }
         return criterias;
     }
