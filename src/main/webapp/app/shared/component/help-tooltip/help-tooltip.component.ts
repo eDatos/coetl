486da0ae0637a9c65bbfe5d5bc23c3fc42fa18ec
Miguel Cabrera <mcabsan@arte-consultores.com>
2018-08-02 14:15:54 +0100

    COETL-34 - Planificación de ETL
    > Modificado componente help-tooltip para permitir indicar el
    posicionamento del tooltip.

--- src/main/webapp/app/shared/component/help-tooltip/help-tooltip.component.ts
+++ src/main/webapp/app/shared/component/help-tooltip/help-tooltip.component.ts
@@ -2,13 +2,12 @@ import { Component, Input } from '@angular/core';
 
 @Component({
     selector: 'ac-help-tooltip',
-    template: '<i class="fa fa-info-circle" placement="right" [ngbTooltip]="label"></i>'
+    template: '<i class="fa fa-info-circle" [placement]="position" [ngbTooltip]="label"></i>'
 })
-
 export class HelpTooltipComponent {
+    @Input() label: string;
 
-    @Input()
-    label: string;
+    @Input() position: 'top' | 'right' | 'left' | 'bottom' = 'right';
 
-    constructor() { }
+    constructor() {}
 }
