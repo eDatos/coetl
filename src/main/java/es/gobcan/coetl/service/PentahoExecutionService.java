5f3339b6edc92e0e109878f9b683a2125b09fb14
Miguel Cabrera <mcabsan@arte-consultores.com>
2018-08-07 08:12:38 +0100

    COETL-35 - Ejecución de ETL
    > Modificado método execute de la clase
    PentahoExecutionService para crear una ejecución asociada a la ETL.

--- src/main/java/es/gobcan/coetl/service/PentahoExecutionService.java
+++ src/main/java/es/gobcan/coetl/service/PentahoExecutionService.java
@@ -1,6 +1,9 @@
 package es.gobcan.coetl.service;
 
+import es.gobcan.coetl.domain.Etl;
+import es.gobcan.coetl.domain.Execution.Type;
+
 public interface PentahoExecutionService {
 
-    void execute(String etlCode);
+    void execute(Etl etl, Type type);
 }
--- src/main/java/es/gobcan/coetl/service/impl/PentahoExecutionServiceImpl.java
+++ src/main/java/es/gobcan/coetl/service/impl/PentahoExecutionServiceImpl.java
@@ -2,8 +2,12 @@ package es.gobcan.coetl.service.impl;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import es.gobcan.coetl.domain.Etl;
+import es.gobcan.coetl.domain.Execution.Type;
+import es.gobcan.coetl.service.ExecutionService;
 import es.gobcan.coetl.service.PentahoExecutionService;
 
 @Service
@@ -11,8 +15,12 @@ public class PentahoExecutionServiceImpl implements PentahoExecutionService {
 
     private static final Logger LOG = LoggerFactory.getLogger(PentahoExecutionServiceImpl.class);
 
+    @Autowired
+    ExecutionService executionService;
+
     @Override
-    public void execute(String etlCode) {
-        LOG.info("Executing ETL : {}", etlCode);
+    public void execute(Etl etl, Type type) {
+        LOG.info("Executing ETL : {}", etl.getCode());
+        executionService.create(etl, type);
     }
 }
