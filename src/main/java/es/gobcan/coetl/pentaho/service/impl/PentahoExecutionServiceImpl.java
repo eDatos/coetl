120d424a7c06cbb6f3cf911a0eaad0aeffe312ff
Miguel Cabrera <mcabsan@arte-consultores.com>
2018-08-08 14:00:37 +0100

    COETL-35 - Ejecución de ETL
    > Creada clase util PentahoUtil con métodos para la obtención de la URL,
    usuario y password del servidor Carte y para la ejecución de diferentes
    opciones en el servidor.
    
    > Modificado método execute de la clase PentahoExecutionServiceImpl
    para comprobar el estado del servidor (mock).

--- src/main/java/es/gobcan/coetl/pentaho/service/impl/PentahoExecutionServiceImpl.java
+++ src/main/java/es/gobcan/coetl/pentaho/service/impl/PentahoExecutionServiceImpl.java
@@ -3,11 +3,18 @@ package es.gobcan.coetl.pentaho.service.impl;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 
+import es.gobcan.coetl.config.PentahoProperties;
 import es.gobcan.coetl.domain.Etl;
 import es.gobcan.coetl.domain.Execution.Type;
+import es.gobcan.coetl.pentaho.dto.ServerStatusDTO;
+import es.gobcan.coetl.pentaho.enumeration.PentahoServerResource;
 import es.gobcan.coetl.pentaho.service.PentahoExecutionService;
+import es.gobcan.coetl.pentaho.service.util.PentahoUtil;
 import es.gobcan.coetl.service.ExecutionService;
 
 @Service
@@ -16,11 +23,20 @@ public class PentahoExecutionServiceImpl implements PentahoExecutionService {
     private static final Logger LOG = LoggerFactory.getLogger(PentahoExecutionServiceImpl.class);
 
     @Autowired
+    PentahoProperties pentahoProperties;
+
+    @Autowired
     ExecutionService executionService;
 
     @Override
     public void execute(Etl etl, Type type) {
         LOG.info("Executing ETL : {}", etl.getCode());
         executionService.create(etl, type);
+        final String url = PentahoUtil.getUrl(pentahoProperties);
+        final String user = PentahoUtil.getUser(pentahoProperties);
+        final String password = PentahoUtil.getPassword(pentahoProperties);
+        final MultiValueMap<String, String> queryParams = new LinkedMultiValueMap<>();
+        queryParams.add("xml", "y");
+        PentahoUtil.execute(user, password, url, PentahoServerResource.STATUS, HttpMethod.GET, queryParams, ServerStatusDTO.class);
     }
 }
---
+++ src/main/java/es/gobcan/coetl/pentaho/service/util/PentahoUtil.java
@@ -0,0 +1,57 @@
+package es.gobcan.coetl.pentaho.service.util;
+
+import java.nio.charset.Charset;
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.codec.Base64;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import es.gobcan.coetl.config.PentahoProperties;
+import es.gobcan.coetl.pentaho.dto.PentahoResponseDTO;
+import es.gobcan.coetl.pentaho.enumeration.PentahoCarteResource;
+
+public final class PentahoUtil {
+
+    private PentahoUtil() {
+    }
+
+    public static <E extends Enum<E> & PentahoCarteResource, T extends PentahoResponseDTO> ResponseEntity<T> execute(String user, String password, String url, E enumResource, HttpMethod httpMethod,
+            MultiValueMap<String, String> queryParams, Class<T> clazz) {
+        String uri = new StringBuilder().append(url).append(enumResource.getResource()).toString();
+        String uriWithQueryParameters = UriComponentsBuilder.fromHttpUrl(uri).queryParams(queryParams).toUriString();
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.exchange(uriWithQueryParameters, httpMethod, new HttpEntity<>(createHeaders(user, password)), clazz);
+    }
+
+    public static String getUrl(PentahoProperties pentahoProperties) {
+        //@formatter:off
+        return new StringBuilder()
+                .append("http://")
+                .append(pentahoProperties.getEndpoint().endsWith("/") ? pentahoProperties.getEndpoint() : pentahoProperties.getEndpoint() + "/")
+                .append("kettle/")
+                .toString();
+        //@formatter:on
+    }
+
+    public static String getUser(PentahoProperties pentahoProperties) {
+        return pentahoProperties.getAuth().getUser();
+    }
+
+    public static String getPassword(PentahoProperties pentahoProperties) {
+        return pentahoProperties.getAuth().getPassword();
+    }
+
+    private static HttpHeaders createHeaders(String username, String password) {
+        String auth = username + ":" + password;
+        byte[] encodedAuth = Base64.encode(auth.getBytes(Charset.forName("US-ASCII")));
+        String authHeader = "Basic " + new String(encodedAuth);
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", authHeader);
+        return headers;
+    }
+}
