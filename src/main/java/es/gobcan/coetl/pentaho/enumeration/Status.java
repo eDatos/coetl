71e153d21a0798a8316bff8ad5478fcfba251ec4
Miguel Cabrera <mcabsan@arte-consultores.com>
2018-08-20 13:58:42 +0100

    COETL-35 - Ejecución de ETL
    > Creada el enumerador Status.

---
+++ src/main/java/es/gobcan/coetl/pentaho/enumeration/Status.java
@@ -0,0 +1,15 @@
+package es.gobcan.coetl.pentaho.enumeration;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlEnumValue;
+
+@XmlEnum(String.class)
+public enum Status {
+    //@formatter:off
+    @XmlEnumValue("Running") RUNNING,
+    @XmlEnumValue("Stopped") STOPPED,
+    @XmlEnumValue("Waiting") WAITING,
+    @XmlEnumValue("Finised") FINISHED,
+    @XmlEnumValue("Finished (with errors)") FINISHED_WITH_ERRORS
+    //@formatter:on
+}
