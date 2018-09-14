30aaeae92e67f93e8ee3ff444740d75ce398a09d
Miguel Ignacio Cabrera San Gil <mcabsan@arte-consultores.com>
2018-09-14 08:40:09 +0100

    COETL-39 - Cambio de estilo de aplicación según contexto de instalación
    > Creada propiedad de configuración internalInstallation en el cliente.

--- src/main/webapp/app/config/generic-config.interface.ts
+++ src/main/webapp/app/config/generic-config.interface.ts
@@ -1,7 +1,8 @@
 export interface GenericConfig {
     cas: {
-        login,
-        logout,
-        service
+        login;
+        logout;
+        service;
     };
-};
+    internalInstallation: string;
+}
