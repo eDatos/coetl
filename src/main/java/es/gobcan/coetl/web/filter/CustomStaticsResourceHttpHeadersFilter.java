0fea1d91b6bb7007eeda0f93699cdaef72787c9a
Miguel Ignacio Cabrera San Gil <mcabsan@arte-consultores.com>
2018-09-20 15:42:08 +0100

    COETL-41 - Corrección de incidencias detectadas de certificación... ...seguridad
    > Se añade los estáticos dentro de templates al filtro
    CustomStaticsResourceHttpHeadersFilter.
    
    > Se elimina el filtro para añadir caché en la cabecera de los ficheros
    estáticos, en su lugar se añade al filtro custom cabeceras para no
    cachear.

--- src/main/java/es/gobcan/coetl/web/filter/CustomStaticsResourceHttpHeadersFilter.java
+++ src/main/java/es/gobcan/coetl/web/filter/CustomStaticsResourceHttpHeadersFilter.java
@@ -14,6 +14,7 @@ public class CustomStaticsResourceHttpHeadersFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
+        // Nothing to set up
     }
 
     @Override
@@ -26,8 +27,10 @@ public class CustomStaticsResourceHttpHeadersFilter implements Filter {
 
         HttpServletResponse httpResponse = (HttpServletResponse) response;
 
+        httpResponse.setHeader("Cache-Control", "no-cache, no-store, max-age=0, must-revalidate");
+        httpResponse.setHeader("Pragma", "no-cache");
+        httpResponse.setIntHeader("Expire", 0);
         httpResponse.setHeader("X-Content-Type-Options", "nosniff");
-        httpResponse.setHeader("X-Frame-Options", "SAMEORIGIN");
         httpResponse.setHeader("X-XSS-Protection", "1; mode=block");
 
         chain.doFilter(request, response);
