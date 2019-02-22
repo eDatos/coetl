c565256863bc796ab356f4c6df0351acdbaa82a8
Javier Villamonte <jvilper@arte-consultores.com>
2019-02-22 16:16:18 +0000

    METAMAC-2887 - Refactor e instalación de demos de aplicación COETL Modernización
    >Se cambia el script de despliegue

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -2,35 +2,43 @@
 
 HOME_PATH=coetl
 TRANSFER_PATH=$HOME_PATH/tmp
-DEPLOY_TARGET_PATH=/servers/coetl/tomcats/coetl01/webapps
 DEMO_ENV=$HOME_PATH/env
+DEPLOY_TARGET_PATH=/servers/metamac/tomcats/metamac01/webapps
+RESTART=1
 
-scp -r etc/deploy deploy@coetl.arte:$TRANSFER_PATH
-scp target/coetl-*.war deploy@coetl.arte:$TRANSFER_PATH/coetl.war
-ssh deploy@coetl.arte <<EOF
+if [ "$1" == "--no-restart" ]; then
+    RESTART=0
+fi
 
-    chmod a+x $TRANSFER_PATH/deploy/*.sh;
+scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
+scp coetl/target/coetl-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/coetl.war
+ssh deploy@estadisticas.arte-consultores.com <<EOF
+
+    # chmod a+x $TRANSFER_PATH/deploy/*.sh;
     
-    sudo service coetl01 stop
+    if [ $RESTART -eq 1 ]; then
+        sudo service metamac01 stop
+    fi
     
     ###
     # COETL
     ###
 
     # Update Process
-    sudo rm -rf $DEPLOY_TARGET_PATH/ROOT
+    sudo rm -rf $DEPLOY_TARGET_PATH/coetl
     sudo mv $TRANSFER_PATH/coetl.war $DEPLOY_TARGET_PATH/coetl.war
-    sudo unzip $DEPLOY_TARGET_PATH/coetl.war -d $DEPLOY_TARGET_PATH/ROOT
+    sudo unzip $DEPLOY_TARGET_PATH/coetl.war -d $DEPLOY_TARGET_PATH/coetl
     sudo rm -rf $DEPLOY_TARGET_PATH/coetl.war
 
     # Restore Configuration
-    sudo cp $DEMO_ENV/logback.xml $DEPLOY_TARGET_PATH/ROOT/WEB-INF/classes/
-    sudo rm -f $DEPLOY_TARGET_PATH/ROOT/WEB-INF/classes/config/application-env.yml
-    sudo cp $DEMO_ENV/data-location.properties $DEPLOY_TARGET_PATH/ROOT/WEB-INF/classes/config/
+    #sudo cp $DEMO_ENV/logback.xml $DEPLOY_TARGET_PATH/coetl/WEB-INF/classes/
+    sudo rm -f $DEPLOY_TARGET_PATH/coetl/WEB-INF/classes/config/application-env.yml
+    sudo cp $DEMO_ENV/data-location.properties $DEPLOY_TARGET_PATH/coetl/WEB-INF/classes/config/
     
-    sudo chown -R coetl.coetl /servers/coetl
-    sudo service coetl01 start
-    
+    if [ $RESTART -eq 1 ]; then
+        sudo chown -R metamac.metamac /servers/metamac
+        sudo service metamac01 start
+    fi
 
     echo "Finished deploy"
 
